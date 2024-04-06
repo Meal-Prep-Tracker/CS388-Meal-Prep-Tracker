678455bd726d00af1865160eb2f33fc5cb92cd84
dustinlaa <drl3@njit.edu>
2024-04-06 15:06:03 -0400

    Configured settings and dark mode

--- app/src/main/java/com/example/mealpreptracker/MainActivity.kt
+++ app/src/main/java/com/example/mealpreptracker/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mealpreptracker
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.DataSnapshot
@@ -13,10 +16,27 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 private const val TAG = "MAIN"
+const val SHARED_PREFS = "SHARED_PREFS"
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sharedpreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        val darkMode = sharedpreferences.getBoolean("darkMode", false)
+        val notifications = sharedpreferences.getBoolean("notifications", true)
+
+        if (darkMode) {
+            // Apply dark theme
+            setTheme(R.style.mealPrepThemeDark)
+        } else {
+            // Apply light theme
+            setTheme(R.style.mealPrepTheme)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
 //        val database = Firebase.database
 //        val myRef = database.getReference("message")
@@ -65,6 +85,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         bottomNavigationView.selectedItemId = R.id.action_settings
+
     }
 
     private fun replaceFragment(mealFragment: Fragment) {
@@ -73,4 +94,5 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.main_layout, mealFragment)
         fragmentTransaction.commit()
     }
+
 }
--- app/src/main/java/com/example/mealpreptracker/SettingsFragment.kt
+++ app/src/main/java/com/example/mealpreptracker/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,6 @@ import android.widget.Button
 import android.widget.Switch
 import androidx.fragment.app.Fragment
 
-const val SHARED_PREFS = "SHARED_PREFS"
 
 class SettingsFragment : Fragment() {
     private lateinit var darkModeSwitch: Switch
@@ -30,16 +30,46 @@ class SettingsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        darkModeSwitch = view.findViewById(R.id.darkModeSwitch)
-        notificationSwitch = view.findViewById(R.id.notificationSwitch)
-        logoutButton = view.findViewById(R.id.logoutButton)
 
         sharedpreferences = requireActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
         val darkMode = sharedpreferences.getBoolean("darkMode", false)
         val notifications = sharedpreferences.getBoolean("notifications", true)
 
-        darkModeSwitch.isChecked = darkMode
-        notificationSwitch.isChecked = notifications
+        val newThemeId = if (darkMode) R.style.mealPrepThemeDark else R.style.mealPrepTheme
+        requireActivity().setTheme(newThemeId)
+
+//        Log.v("Settings", notifications.toString())
+
+
+        return view
+    }
+    companion object {
+        fun newInstance(): SettingsFragment {
+            return SettingsFragment()
+        }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        darkModeSwitch = view.findViewById(R.id.darkModeSwitch)
+        notificationSwitch = view.findViewById(R.id.notificationSwitch)
+        logoutButton = view.findViewById(R.id.logoutButton)
+
+        val darkMode = sharedpreferences.getBoolean("darkMode", false)
+        val notifications = sharedpreferences.getBoolean("notifications", true)
+
+        if (darkMode) {
+            darkModeSwitch.setChecked(true)
+        } else {
+            darkModeSwitch.setChecked(false)
+        }
+
+        if (notifications) {
+            notificationSwitch.setChecked(true)
+        } else {
+            notificationSwitch.setChecked(false)
+        }
 
         // Dark mode switch listener
         darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
@@ -49,7 +79,15 @@ class SettingsFragment : Fragment() {
                 apply()
             }
             // Apply the appropriate theme
-            applyTheme(isChecked)
+            Log.v("Settings", "isCheckedVal $isChecked")
+            if (isChecked) {
+                // Apply dark theme
+                requireActivity().setTheme(R.style.mealPrepThemeDark)
+            } else {
+                // Apply light theme
+                requireActivity().setTheme(R.style.mealPrepTheme)
+            }
+            requireActivity().recreate() //
         }
 
 
@@ -66,23 +104,7 @@ class SettingsFragment : Fragment() {
 //            startActivity(Intent(this, WelcomeActivity::class.java))
         }
 
-        return view
-    }
-    companion object {
-        fun newInstance(): SettingsFragment {
-            return SettingsFragment()
-        }
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
     }
 
-    private fun applyTheme(isDarkMode: Boolean) {
-        val themeId = if (isDarkMode) {
-            R.style.mealPrepThemeDark
-        } else {
-            R.style.mealPrepTheme
-        }
-        requireActivity().setTheme(themeId)
-    }
 }
