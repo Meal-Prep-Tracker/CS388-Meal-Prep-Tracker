0d0cc5d9e64737c5afbe4a03d6df961b116dc0b2
Rahul Shah <rahulnshah3012@gmail.com>
2024-04-09 22:51:53 -0400

    changed @SerialName to @SerializedName, removed unnecessary imports

--- app/src/main/java/com/example/mealpreptracker/NutritionSummary.kt
+++ app/src/main/java/com/example/mealpreptracker/NutritionSummary.kt
@@ -1,43 +1,30 @@
 package com.example.mealpreptracker
-import android.support.annotation.Keep
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-@Keep
-@Serializable
+import com.google.gson.annotations.SerializedName
+
+
 data class NutritionSummary (
-    @SerialName("calories")
+    @SerializedName("calories")
     val calories: Double?  = 0.0,
-    @SerialName("protein_g")
-    val protein_g: Double?  = 0.0,
-    @SerialName("carbohydrates_total_g")
-    val carbohydrates_total_g: Double? = 0.0,
-    @SerialName("fat_total_g")
-    val fat_total_g: Double? = 0.0,
-    @SerialName("fat_saturated_g")
-    val fat_saturated_g: Double? = 0.0,
-    @SerialName("sodium_mg")
-    val sodium_mg: Double? = 0.0,
-    @SerialName("potassium_mg")
-    val potassium_mg: Double? = 0.0,
-    @SerialName("cholesterol_mg")
-    val cholesterol_mg: Double? = 0.0,
-    @SerialName("fiber_g")
-    val fiber_g: Double? = 0.0,
-    @SerialName("sugar_g")
-    val sugar_g: Double? = 0.0
+    @SerializedName("protein_g")
+    val protein: Double?  = 0.0,
+    @SerializedName("carbohydrates_total_g")
+    val carbohydrates: Double? = 0.0,
+    @SerializedName("fat_total_g")
+    val fat: Double? = 0.0,
+    @SerializedName("fat_saturated_g")
+    val saturatedFat: Double? = 0.0,
+    @SerializedName("sodium_mg")
+    val sodium: Double? = 0.0,
+    @SerializedName("potassium_mg")
+    val potassium: Double? = 0.0,
+    @SerializedName("cholesterol_mg")
+    val cholesterol: Double? = 0.0,
+    @SerializedName("fiber_g")
+    val fiber: Double? = 0.0,
+    @SerializedName("sugar_g")
+    val sugar: Double? = 0.0
 ):  java.io.Serializable
 
-//@Keep
-//@Serializable
-//data class IngredientResponse(
-//    @SerialName("response")
-//    val response: BaseResponse?
-//)
-@Keep
-@Serializable
-data class BaseResponse(
-    @SerialName("items")
-    val items: List<NutritionSummary>?
-)
+
 
